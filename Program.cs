aae943f958f942596e1725b3f0214bfaebcb8c34
tolyale316-commits <tolya.le316@gmail.com>
2026-02-17 23:46:24 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,30 +6,31 @@ class Program
     {
         Console.WriteLine("=== Демонстрация работы класса TextFile ===\n");
         
-        // Создание объектов
-        TextFile file1 = new TextFile();
-        TextFile file2 = new TextFile("документ", "docx", 150.5, "Windows-1251");
+        // Создание объектов через разные конструкторы
+        Console.WriteLine("1. Создание объектов:");
+        TextFile file1 = new TextFile(); // конструктор по умолчанию
+        TextFile file2 = new TextFile("документ", "docx", 150.5, "Windows-1251"); // конструктор с параметрами
         
-        // Показ информации
-        Console.WriteLine("Информация о файле 2:");
-        string info;
-        file2.ShowInfo(out info);
+        // Демонстрация метода ShowInfo с out параметром
+        Console.WriteLine("\n2. Информация о файле 2:");
+        string detailedInfo;
+        file2.ShowInfo(out detailedInfo);
         
-        // Переименование
-        Console.WriteLine("\nПереименовываем файл:");
+        // Демонстрация метода Rename (обычный параметр)
+        Console.WriteLine("\n3. Переименовываем файл:");
         file2.Rename("новый_документ");
         
-        // Изменение кодировки
-        Console.WriteLine("\nМеняем кодировку:");
+        // Демонстрация метода ChangeEncoding с ref параметром
+        Console.WriteLine("\n4. Изменяем кодировку:");
         bool success = false;
         file2.ChangeEncoding("UTF-16", ref success);
-        Console.WriteLine($"Успешно: {success}");
+        Console.WriteLine($"Операция успешна: {success}");
         
-        // Применение обновлений
-        Console.WriteLine("\nПрименяем обновления:");
-        file2.ApplyUpdates("Обновление 1", "Обновление 2");
+        // Демонстрация метода ApplyUpdates с params параметром
+        Console.WriteLine("\n5. Применяем обновления:");
+        file2.ApplyUpdates("Обновление метаданных", "Проверка орфографии", "Индексация");
         
-        Console.WriteLine("\nНажмите любую клавишу...");
+        Console.WriteLine("\nПрограмма завершена. Нажмите любую клавишу...");
         Console.ReadKey();
     }
 }
